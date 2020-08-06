72c6c64d30271f42591e85b645bc2bbaaecac63f
Kai Xiang Yong <z5175681@student.unsw.edu.au>
2020-08-06 19:54:10 +1000

    implemented while loop and test statement

--- sheeple.pl
+++ sheeple.pl
@@ -1,10 +1,10 @@
 #!/usr/bin/perl -w
 
-@dash_cmd = ("ls","pwd","id","date");
 $in_function = 0;
 
 while (<>){
     chomp $_;
+    # TODO Split ;
     $indent = get_indentation($_);
     my $line = translate($_);
     $line = replace_argv($line);
@@ -46,16 +46,25 @@ sub translate {
     # Variable Assignment
     $line = translate_var_assignment($_);
     return $line if $line ne "";
+    # If Statement
+    $line = translate_if_statement($_);
+    return $line if $line ne "";
     # For loop
     $line = translate_for_loop($_);
     return $line if $line ne "";
-    $line = translate_if_statement($_);
+    $line = translate_while_loop($_);
     return $line if $line ne "";
+    # Else
+    $line = translate_unknown_cmd($_) if $line ne "";
+    return $line;
+}
 
+sub translate_unknown_cmd {
+    return "system \"$1\";";
 }
 
 sub translate_sys_cmd {
-    # =============== echo =============== 
+    # =============== echo ===============
     # Case 1: Double quote
     if (/^echo "(.*)"/) {
         my $string = $1;
@@ -63,7 +72,7 @@ sub translate_sys_cmd {
         $string =~ s/\\\\"/\\"/g;
         return "print \"$string\\n\";";
     }
-    # Case 2: Single quote 
+    # Case 2: Single quote
     if (/^echo '(.*)'$/) {
         my $string = $1;
         $string =~ s/'\\''/'/gi;
@@ -73,17 +82,12 @@ sub translate_sys_cmd {
     # Case 3: No quote
     return "print \"$1\\n\";" if /^echo (.*)$/;
 
-    # =============== cd =============== 
+    # =============== cd ===============
     return "chdir '$1';" if /^cd (.*)/;
 
     # =============== exit ===============
     return $_ . ";" if /^exit/;
     return "\$$1 = <STDIN>;" . indent("chomp \$$1;") if /^read (.*)/;
-
-    # =============== etc =============== 
-    foreach $cmd (@dash_cmd) {
-        return "system \"$_\";" if /^($cmd)(\s.+)?/;
-    }
 }
 
 sub translate_header {
@@ -116,15 +120,24 @@ sub translate_if_statement {
     return "" if /^then/;
     my ($line) = @_;
     if (/^if (.*)/) {
-        my $condition = parse_condition($1);
+        my $condition = parse_test_statement($1);
         return "if ($condition) {";
     } elsif (/^elif (.*)/) {
-        my $condition = parse_condition($1);
+        my $condition = parse_test_statement($1);
         return "} elsif ($condition) {";
     } elsif (/^else/) {
         return "} else {";
     } elsif (/^fi/) {
         return "}";
+    }
+}
+
+# TODO: Subset 2 
+sub translate_while_loop {
+    my ($line) = @_;
+    if (/^while (.*)/) {
+        my $condition = parse_test_statement($1);
+        return "while ($condition) {";
     }
 }
 
@@ -159,13 +172,66 @@ sub replace_argv {
     }
 }
 
+sub parse_test_statement {
+    my ($statement) = @_;
+    if ($statement =~ /^test (.*)$/){
+        return parse_condition($1);
+    } elsif ($statement =~ /\[ (.*) \]/){
+        return parse_condition($1);
+    }
+    my $other = parse_condition($1);
+    return $other if $other ne "";
+}
+
 sub parse_condition {
     my ($condition) = @_;
-    # String Compare
-    if ($condition =~ /^test (.+) = (.+)/) {
-        return "'$1' eq '$2'";
+    
+    # True / False
+    if    ($condition eq "true") { return "1"; }
+    elsif ($condition eq "false") { return "0"; } 
+
+    # 2 Argument comparison
+    $condition =~ /^(.+) (.+) (.+)/;
+    if ($3){
+        # TODO: Parameter process Variable / String / Int ?
+        my $p1 = parse_condition_args($1);
+        my $p2 = parse_condition_args($3);
+        # String
+        if    ($2 eq "=")   { return "$p1 eq $p2"; }    #  ()  =  ()  ->  () eq ()
+        elsif ($2 eq "!=")  { return "$p1 ne $p2"; }    #  ()  != ()  ->  () ne ()
+        # Numeric
+        elsif ($2 eq "-eq") { return "$p1 == $p2"; }    #  () -eq ()  ->  () == ()
+        elsif ($2 eq "-ge") { return "$p1 >= $p2"; }    #  () -ge ()  ->  () >= ()
+        elsif ($2 eq "-gt") { return "$p1 > $p2";  }    #  () -gt ()  ->  () >  ()
+        elsif ($2 eq "-le") { return "$p1 <= $p2"; }    #  () -le ()  ->  () <= ()
+        elsif ($2 eq "-lt") { return "$p1 < $p2";  }    #  () -lt ()  ->  () <  ()
+        elsif ($2 eq "-ne") { return "$p1 != $p2"; }    #  () -ne ()  ->  () != ()
+    } else {
+        $condition =~ /^(.+) (.+)/;
+        # TODO: Parameter process Variable / String / Int?
+        my $p = parse_condition_args($2);
+        if    ($1 eq "-n") { return "$p != \"\""; }     # -n ()  ->  () != ""
+        elsif ($1 eq "-z") { return "$p == \"\""; }     # -z ()  ->  () == ""
+        # TODO:  operation
+        #       -f ()
+        #       -d ()
+        if ($1 eq "-r" || 
+            $1 eq "-d" ||
+            $1 eq "-f ") { return "$1 $p"; }
     }
-    # TODO: Numeric compare
-    # TODO: File operation
-    # TODO: operator !
+
+    # Subset 4
+    # && 
+    # || Operation
+}
+
+sub parse_condition_args {
+    my ($param) = @_;
+    if ($param =~ /^\$(.*)/){
+        return $param;
+    } elsif ($param =~ /['"]/) {
+        return "quote not implemented";
+    } else {
+        return "\'$param\'";
+    }
 }
